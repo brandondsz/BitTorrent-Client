b7c4019d604a66ef5c9b11a52b58a62a035f5b06
Brandon Dsouza <brandon.dsouza@onscreen.us>
2024-10-21 19:05:00 +0530

    restructure - decode start & end index

---
+++ BitTorrent-Client/Bencode.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Client
+{
+    public class Bencode
+    {
+        
+        public static object DecodeElement(string encodedValue, int index, out int endIndex)
+        {
+            int _endIndex;
+            object decodedValue = encodedValue[index] switch
+            {
+                //<length>:<contents>
+                var target when char.IsDigit(target) =>
+                    DecodeString(encodedValue, index, out _endIndex),
+                //i<number>e
+                'i' => DecodeInteger(encodedValue, index + 1, out _endIndex),
+                //l<bencoded_elements>e
+                'l' => DecodeList(encodedValue, index + 1, out _endIndex),
+                //d<key1><value1>...<keyN><valueN>e
+                'd' => DecodeDictionary(encodedValue, index + 1, out _endIndex),
+                _ => throw new InvalidOperationException("Invalid encoded value: " + encodedValue)
+            };
+            endIndex = _endIndex;
+            return decodedValue;
+        }
+
+        static string DecodeString(string encodedValue, int firstDigitDetected, out int endIndex)
+        {
+            var colonIndex = encodedValue.IndexOf(':', firstDigitDetected);
+            var length =
+                int.Parse(encodedValue[firstDigitDetected..colonIndex].ToString());
+            var startIndex = colonIndex + 1;
+            endIndex = startIndex + length;
+            var strValue = encodedValue[startIndex..endIndex];
+            return strValue;
+        }
+
+
+        static long DecodeInteger(string encodedValue, int startIndex, out int endIndex)
+        {
+            endIndex = encodedValue.IndexOf('e', startIndex);
+            if (endIndex != -1)
+            {
+                var strValue = encodedValue[startIndex..endIndex];
+                endIndex++;
+                return long.Parse(strValue);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid encoded value: " +
+                                                    encodedValue);
+            }
+        }
+
+        static List<object> DecodeList(string encodedValue, int startIndex, out int endIndex)
+        {
+            var result = new List<object>();
+            var index = startIndex;
+            while (index < encodedValue.Length)
+            {
+                if (encodedValue[index] == 'e')
+                {
+                    break;
+                }
+                var decodedValue =
+                    DecodeElement(encodedValue, index, out int separatorIndex);
+                result.Add(decodedValue);
+                index = separatorIndex;
+            }
+            endIndex = index + 1;
+            return result;
+        }
+
+
+        static Dictionary<string, object> DecodeDictionary(string encodedValue, int startIndex, out int endIndex)
+        {
+            var result = new Dictionary<string, object>();
+            var index = startIndex;
+            var count = 0;
+            while (index < encodedValue.Length)
+            {
+                count++;
+                if (encodedValue[index] == 'e')
+                {
+                    break;
+                }
+                var key = DecodeString(encodedValue, index, out int keyEndIndex);
+                var value =
+                    DecodeElement(encodedValue, keyEndIndex, out int separatorIndex);
+                result.Add(key, value);
+                index = separatorIndex;
+            }
+            endIndex = index + 1;
+            return result;
+        }
+    }
+};
--- BitTorrent-Client/Program.cs
+++ BitTorrent-Client/Program.cs
@@ -1,9 +1,14 @@
-﻿using System.Text;
+﻿using Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace Client
+namespace BitTorrent_Client
 {
-    public class Bencode
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -19,105 +24,8 @@ namespace Client
             // Parse command and act accordingly
             if (command == "decode")
             {
-                Console.WriteLine(JsonSerializer.Serialize(Decode(param)));
+                Console.WriteLine(JsonSerializer.Serialize(Bencode.DecodeElement(param, 0, out _)));
             }
-        }
-        public static object Decode(string input)
-        {
-            switch (input[0])
-            {
-                case var c when Char.IsDigit(c):
-                    return DecodeString(input);
-                case 'i':
-                    return DecodeInt(input);
-                case 'l':
-                    return DecodeList(input);
-                case 'd':
-                    return DecodeDictionary(input);
-                default:
-                    throw new InvalidOperationException($"invalid encoded value: {input}");
-            }
-        }
-        private static string DecodeString(string input)
-        {
-            int colonIndex = input.IndexOf(":");
-            if (colonIndex == -1)
-            {
-                throw new InvalidOperationException($"Invalid encoded string: {input}");
-            }
-            int lenght = int.Parse(input[..colonIndex]);
-            return input.Substring(colonIndex + 1, lenght);
-        }
-        private static long DecodeInt(string input)
-        {
-            int endIndex = input.IndexOf('e', 1);
-            if (endIndex == -1)
-            {
-                throw new InvalidOperationException(
-                    $"Invalid encoded integer: {input}");
-            }
-            string number = input[1..endIndex];
-            return long.Parse(number);
-        }
-        private static object[] DecodeList(string input)
-        {
-            List<object> elements = new();
-            input = input[1..^1];
-            while (input.Length > 0 && input[0] != 'e')
-            {
-                object element = Decode(input);
-                elements.Add(element);
-                string encodedElement = Encode(element);
-                input = input[encodedElement.Length..];
-            }
-            return elements.ToArray();
-        }
-        private static Dictionary<string, object> DecodeDictionary(string input)
-        {
-            input = input[1..];
-            var result = new Dictionary<string, object>();
-            while (input.Length > 0 && input[0] != 'e')
-            {
-                var key = Decode(input);
-                input = input[Encode(key).Length..];
-                var value = Decode(input);
-                input = input[Encode(value).Length..];
-                if (key is string keyStr)
-                {
-                    result[keyStr] = value;
-                }
-                else
-                {
-                    throw new Exception("Dictionary key must be a string");
-                }
-            }
-            return result;
-        }
-        private static string EncodeNonPrimitiveType(object input)
-        {
-            if (input is object[] inputArray)
-            {
-                return $"l{string.Join("", inputArray.Select(x => Encode(x)))}e";
-            }
-            else if (input is Dictionary<string, object> inputDictionary)
-            {
-                return $"d{string.Join("", inputDictionary.Values.Select(x => Encode(x)))}e";
-            }
-            else
-            {
-                throw new Exception($"Unknown type: {input.GetType().FullName}");
-            }
-        }
-        public static string Encode(object input)
-        {
-            return input switch
-            {
-                long n => $"i{n}e",
-                string s => $"{s.Length}:{s}",
-                object[] arr => $"l{string.Join("", arr.Select(Encode))}e",
-                object obj => EncodeNonPrimitiveType(input),
-                _ => throw new Exception($"Unknown type: {input.GetType().FullName}")
-            };
         }
     }
-};
+}
